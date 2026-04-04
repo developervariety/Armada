724b4cc6d9a7d731d8f46eeebaa58dcc08c3530d
Joel Christner <joel.christner@gmail.com>
2026-04-03 20:09:37 -0700

    Add Armada.Proxy Docker build script

---
+++ build-proxy.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set TAG=%~1
+
+if "%TAG%"=="" (
+    echo Building jchristn77/armada-proxy:latest
+    docker buildx build ^
+        --platform linux/amd64,linux/arm64/v8 ^
+        -f src/Armada.Proxy/Dockerfile ^
+        -t jchristn77/armada-proxy:latest ^
+        --push ^
+        .
+) else (
+    echo Building jchristn77/armada-proxy:latest and jchristn77/armada-proxy:%TAG%
+    docker buildx build ^
+        --platform linux/amd64,linux/arm64/v8 ^
+        -f src/Armada.Proxy/Dockerfile ^
+        -t jchristn77/armada-proxy:latest ^
+        -t jchristn77/armada-proxy:%TAG% ^
+        --push ^
+        .
+)
